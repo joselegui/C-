c90c6e5504872b599c0165e20d3c1a07ec9df009
joselegui <leguizamonjoseramon@yahoo.com.ar>
2019-06-17 21:49:24 -0300

    Contador de caracteres en C#
    Visual studio 2010

---
+++ Caracteres/Caracteres/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Caracteres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txt_textos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelAyuda = new System.Windows.Forms.Label();
+            this.labelMinimizar = new System.Windows.Forms.Label();
+            this.labelExit = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptar.Location = new System.Drawing.Point(94, 103);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(147, 46);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // txt_textos
+            // 
+            this.txt_textos.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_textos.Location = new System.Drawing.Point(35, 54);
+            this.txt_textos.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_textos.Multiline = true;
+            this.txt_textos.Name = "txt_textos";
+            this.txt_textos.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_textos.Size = new System.Drawing.Size(265, 45);
+            this.txt_textos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese el texto que desee";
+            // 
+            // labelAyuda
+            // 
+            this.labelAyuda.AutoSize = true;
+            this.labelAyuda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAyuda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelAyuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAyuda.Location = new System.Drawing.Point(313, 139);
+            this.labelAyuda.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelAyuda.Name = "labelAyuda";
+            this.labelAyuda.Size = new System.Drawing.Size(19, 19);
+            this.labelAyuda.TabIndex = 3;
+            this.labelAyuda.Text = "?";
+            this.labelAyuda.Click += new System.EventHandler(this.labelAyuda_Click);
+            // 
+            // labelMinimizar
+            // 
+            this.labelMinimizar.AutoSize = true;
+            this.labelMinimizar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelMinimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelMinimizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMinimizar.Location = new System.Drawing.Point(290, 2);
+            this.labelMinimizar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelMinimizar.Name = "labelMinimizar";
+            this.labelMinimizar.Size = new System.Drawing.Size(16, 19);
+            this.labelMinimizar.TabIndex = 4;
+            this.labelMinimizar.Text = "-";
+            this.labelMinimizar.Click += new System.EventHandler(this.labelMinimizar_Click);
+            // 
+            // labelExit
+            // 
+            this.labelExit.AutoSize = true;
+            this.labelExit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelExit.Location = new System.Drawing.Point(311, 2);
+            this.labelExit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelExit.Name = "labelExit";
+            this.labelExit.Size = new System.Drawing.Size(20, 19);
+            this.labelExit.TabIndex = 5;
+            this.labelExit.Text = "X";
+            this.labelExit.Click += new System.EventHandler(this.labelExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(335, 159);
+            this.Controls.Add(this.labelExit);
+            this.Controls.Add(this.labelMinimizar);
+            this.Controls.Add(this.labelAyuda);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_textos);
+            this.Controls.Add(this.btnAceptar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caracteres";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txt_textos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelAyuda;
+        private System.Windows.Forms.Label labelMinimizar;
+        private System.Windows.Forms.Label labelExit;
+    }
+}
+
---
+++ Caracteres/Caracteres/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Caracteres
+{
+    public partial class Form1 : Form
+    {
+
+        DialogResult result;
+        public string cadena;
+        public int vocales = 0, contador = 0, palabras = 0, contador2 = 0;
+
+        //Variables para mover el formulario.
+        Point Posiciondelformulario;
+        Boolean mouse;
+
+        public Form1()
+        {         
+            InitializeComponent();
+            txt_textos.Select();
+        }
+        //Desde aca se cierra la aplicacion.
+        private void labelExit_Click(object sender, EventArgs e)
+        {
+            result =MessageBox.Show("¡Seguro que desea salir de la aplicación?", "¡Advertencia!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+                this.Close();
+        }
+        //Desde aca se miniminiza la aplicacion
+        private void labelMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //Ace se esncuentra toda las funciones de la aplicacion...
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+                //Declaracion de variables
+                cadena = txt_textos.Text;
+
+                //Hace el recorrido del texto ingresado obteniendo la longitud.
+                for (int i = 0; i < cadena.Length; i++)
+                {
+                    contador++;
+                }
+
+                for (int i = 0; i < cadena.Length; i++)
+                {   //Condicion que evalua la cadna ingresada en busca de los caracteres A o a
+                    if ((cadena[i] == 'a') || (cadena[i] == 'A'))
+                    {
+                        vocales++;
+                    }
+                }
+
+                result = MessageBox.Show("La cantidad de letras 'a' o 'A' son: " + vocales + "\n¿Desea saber la cantidad de caracteres, que tiene lo escrito?", "Contador de letras A o a", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                if (result == DialogResult.Yes)
+                {
+                    cadena = cadena.Replace(" ", "");//Elimina los espacios vacios para que no los cuente como caracteres...
+                    result = MessageBox.Show("La cantidad de caracteres son: " + cadena.Length + "\n¿Desea guardar lo escrito en formato *.txt?", "Contador de caracteres", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                    //Condicion en donde me guarda en un archivo txt, lo escrito en el textbox.
+                    if (result == DialogResult.Yes)
+                    {
+                        StreamWriter escribir = null;
+                        escribir = File.CreateText("Contador de caracteres.txt");
+                        escribir.WriteLine(txt_textos.Text);
+                        escribir.Flush();
+                        escribir.Close();
+                        limpiartexto();
+                        result = MessageBox.Show("El texto fue guardado con exito" + "\ncon el nombre de: Contador de caracteres.txt" + "\n¿Quiere saber cuantas palabras tiene el archivo?", "Guardar Archivo ", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                    }
+                    //Condicion y variables para leer el archivo txt
+                    if (result == DialogResult.Yes)
+                    {   //variable para leer el archivo
+                        string leerpalabras;
+                        int aux = 0, contarpalabras = 0, resultado = 0;
+
+                        TextReader leer;
+                        //instancia para leer y buscar el archivo txt
+
+                        leer = new StreamReader("Contador de caracteres.txt");
+                        //leer.ReadToEnd();
+                        leerpalabras = Convert.ToString(leer.ReadToEnd());
+
+                        resultado = palabrascontadas(leerpalabras + ' ', aux, contarpalabras);
+
+
+                        MessageBox.Show("El archivo contiene " + resultado.ToString() + " palabras", "Contador de palabras", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);//ReadToEnd = metodo para leer todo el archivo
+                        leer.Close();
+                    }
+                    if (result == DialogResult.Yes){
+                        MessageBox.Show("La aplicacion a finalizado con exito "
+                            + "\n\n¡¡Hasta pronto :) !!", "Fin del programa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+        }
+
+        //Metodo para la funciones de contar solo las palabras del archivo....
+        private int palabrascontadas(string cadena, int auxiliar, int palabras)
+        {
+            if (auxiliar >= cadena.Length - 1)
+            {
+                return palabras + 1;
+            }
+            else {
+                if (cadena[auxiliar] == ' ' && cadena[auxiliar + 1] != ' ')
+                    palabras++;
+                return palabrascontadas(cadena, auxiliar+1,palabras);
+            }
+        }
+
+        //Metodo para limpiar el textbox
+        public void limpiartexto() {
+            txt_textos.Clear();
+        }
+
+        //Ayuda: explicación de lo que ace la aplicación
+        private void labelAyuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("En esta aplicación se genera varios pasos:"+
+                "\n1: Cuantas veces se repite la letra A o a."+
+                "\n2: Se cuentan los caracteres del TextBox."+
+                "\n3: Se guarda lo escrito en un archivo txt."+
+                "\n4: Se cuentan las palabras guardadas en el txt."+
+                "\nSe pueden calcelar las funciones cuando Uds lo deseen..."+
+                "\n\n¡¡ Buena suaerte ;) !!","AYUDA",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        // Evento KeyPress de la caja de texto, para validar solo caracteres.
+        //private void Sololetras(object sender, KeyPressEventArgs e)
+        //{
+        //    if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Space) && (e.KeyChar != (char)Keys.Back)
+        //        && (e.KeyChar != (char)Keys.Enter))
+        //    {
+        //        MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //        e.Handled = true;
+        //        return;
+        //    }
+        //}
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Posiciondelformulario = new Point(Cursor.Position.X - Location.X, Cursor.Position.Y - Location.Y);
+            mouse = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouse == true)
+            {
+                Location = new Point(Cursor.Position.X - Posiciondelformulario.X, Cursor.Position.Y - Posiciondelformulario.Y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = false;
+        }
+    }
+}
